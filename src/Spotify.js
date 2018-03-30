a43408d5e0d4167edb1d0c558e3e1a52ca8a8a8f
Gregory Menezes <gregoryfontenele@gmail.com>
2018-03-30 15:45:20 -0300

    Add static layout, gfm-spotify-wrapper, jsdom, AlbumList component and initial tests

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'gfm-spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  apiToken: 'my token',
+});
+
+export default spotify;
---
+++ tests/Spotify.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import spotify from '../src/Spotify';
+
+describe('Spotify', () => {
+  it('should be an object', () => {
+    expect(spotify).to.be.an('object');
+  });
+
+  it('should have search method', () => {
+    expect(spotify.search).to.exist;
+  });
+
+});
