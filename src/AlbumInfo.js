7fe6e5482a46a84b824d9f84f5a698b156659624
Gregory Menezes <gregoryfontenele@gmail.com>
2018-03-31 15:50:04 -0300

    Add albumtracks to interface

--- src/AlbumInfo.js
+++ src/AlbumInfo.js
@@ -1,12 +1,12 @@
 function createMarkup( data ) {
   return `
-  <div class="album-info">
-    <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
-    <p class="album-title">${data.name}</p>
-    <p class="album-artist">${data.artists[0].name}</p>
-    <p class="album-counter">${data.tracks.total} Músicas</p>
-  </div>
-`;
+    <div class="album-info">
+      <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
+      <p class="album-title">${data.name}</p>
+      <p class="album-artist">${data.artists[0].name}</p>
+      <p class="album-counter">${data.tracks.total} Músicas</p>
+    </div>
+  `;
 }
 
 export default function renderAlbumInfo( data, element ) {
--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
 
 const albums = spotify.search.albums('Incubus');
 const albumList = document.getElementById('album-list');
 
-
 const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
 const albumInfo = document.getElementById('album-info');
+
+const tracks = spotify.album.getTracks('6peEdPVO73WtgGah5sEhX4');
+const albumTracks = document.getElementById('album-musics');
 
 albums
   .then(data => renderAlbums(data.albums.items, albumList));
 
-console.log(album)
 album
   .then(data => renderAlbumInfo(data, albumInfo));
+
+tracks
+  .then(data => renderAlbumTracks( data.items, albumTracks ));
